438b19437473a7a165b753854972b79e4ab4b5fd
Sashitha <sasitha.j@theavo.net>
2023-06-24 01:33:49 +0530

    My Details Updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,66 +1,70 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    siteUrl: `https://sashithachamara.com/`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Sashitha Chamara',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Sashitha Chamara | Full-Stack Developer`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: `Entrepreneur • Full stack developer • Traveler`,
     // Optional: Twitter account handle
-    author: `@rfitzio`,
+    author: ``,
     // Optional: Github account URL
-    github: `https://github.com/ryanfitzgerald`,
+    github: `https://github.com/sashithacj`,
     // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    linkedin: `https://www.linkedin.com/in/sashithachamara/`,
     // Content of the About Me section
-    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
+    about: `Enthusiastic Software / Mobile Apps developer who is keen to explore the newest in the field of technology with 6+ years experience. Hope to be an artisan for Software Engineering.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
       {
-        name: 'Devfolio',
+        name: 'ReactNativeCmdEase',
         description:
-          'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+          'React native project manager written in C#',
+        link: 'https://github.com/sashithacj/ReactNativeCmdEase',
       },
       {
-        name: 'ChromeExtensionKit',
+        name: 'Screen Recorder',
         description:
-          'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
-        link: 'https://chromeextensionkit.com/?ref=devfolio',
-      },
-      {
-        name: 'Another Cool Project',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+          'Screenshot capturing and screen video recording All-in-one program written in C#',
+        link: 'https://github.com/sashithacj/ScreenRecorderBySashitha',
       },
     ],
     // Optional: List your experience, they must have `name` and `description`. `link` is optional.
     experience: [
       {
-        name: 'Acme Corp',
-        description: 'Full-Stack Developer, February 2020 - Present',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Avonet Technologies, Kent Town, Australia',
+        description: 'Senior Software Engineer (React Native), June 2023 - Present',
+        link: '',
       },
       {
-        name: 'Globex Corp',
-        description: 'Full-Stack Developer, December 2017 - February 2020',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Yara Technologies (PVT) Ltd, Narahenpita, Sri Lanka',
+        description: 'Senior Software Engineer (Frontend), January 2022 - June 2023',
+        link: '',
       },
       {
-        name: 'Hooli',
-        description: 'Full-Stack Developer, May 2015 - December 2017',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'NextGen Apps LLC, Maryland, United States',
+        description: 'CEO, Lead Developer Remote, September 2020 - December 2021',
+        link: '',
       },
+      {
+        name: 'Lak United Associate (PVT) LTD, Wattala, Sri Lanka',
+        description: 'Mobile Application Developer, February 2019 - July 2020',
+        link: '',
+      },
+      {
+        name: 'WORA Solutions (PVT) LTD, Negombo, Sri Lanka',
+        description: 'Software Engineer Trainee, September 2018 - November 2018',
+        link: '',
+      }
     ],
     // Optional: List your skills, they must have `name` and `description`.
     skills: [
       {
         name: 'Languages & Frameworks',
         description:
-          'JavaScript (ES6+), Golang, Node.js, Express.js, React, Ruby on Rails, PHP',
+          'React, React Native, Angular, PHP (Laravel), JavaScript (ES6+), Golang, Node.js, Python, C#',
       },
       {
         name: 'Databases',
